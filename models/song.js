faa6383109d5fde4ad5f4e081760ba95f0669332
Vivien <bihuiy7@gmail.com>
2025-08-11 19:40:47 +1000

    complete the Song model

---
+++ models/song.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+  title: { type: String, required: [true, `You need to add a song title.`] },
+  artist: { type: String, required: [true, `You need to add a artist.`] },
+  duration: { type: String },
+  album: { type: String },
+  userLikes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+});
+
+const Song = mongoose.model("Song", songSchema);
+
+export default Song;
