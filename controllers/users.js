7a6bd0177b1687f0eaded618016b647e2e41dbfa
JMaxRowe <jmaxrowe@gmail.com>
2025-08-11 22:27:05 +0100

    changed password confirmation error so it appears in the right spot

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.post('/sign-up', async (req, res, next) =>{
     try {
         if (req.body.password !== req.body.passwordConfirmation) {
-            throw new InvalidData('Passwords do not match.', 'password')
+            throw new InvalidData('Passwords do not match.', 'passwordConfirmation')
     }
 
     const newUser = await User.create(req.body)
