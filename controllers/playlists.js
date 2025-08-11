36d2ce261f8e6be5c7840d2cdd266b2540a5487d
JMaxRowe <jmaxrowe@gmail.com>
2025-08-11 12:18:11 +0100

    added get playlist and playlists and a basic create playlist

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -5,8 +5,9 @@ import { NotFound } from '../utils/errors.js'
 const router = express.Router()
 
 //create playlist - NEED TO SEE IMAGE UPLOAD LESSON TO COMPLETE
-router.post("", async(req, res, next) =>{
+router.post("/", async(req, res, next) =>{
     try {
+        req.body.owner = req.owner
         const playlist = await Playlist.create(req.body)
         return res.status(201).json(playlist)
     } catch (error) {
@@ -14,9 +15,25 @@ router.post("", async(req, res, next) =>{
     }
 })
 //get all playlists
-
+router.get("/", async(req, res, next) =>{
+    try {
+        const playlists = await Playlist.find()
+        return res.status(200).json(playlists)
+    } catch (error) {
+        next(error)
+    }
+})
 // get individual playlist
-
+router.get("/:playlistId", async(req, res, next) =>{
+    try {
+        const playlistId = req.params
+        const playlist = await Playlist.findById(playlistId)
+        if(!playlist) throw new NotFound()
+        return res.status(200).json(playlist)
+    } catch (error) {
+        next(error)
+    }
+})
 // update playlsit
 
 // delete playlist
