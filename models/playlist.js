67fb9027da0039738ce8a4db888413f343f239a4
JMaxRowe <jmaxrowe@gmail.com>
2025-08-11 10:34:12 +0100

    updated playlist model to include playlist attributes

--- models/playlist.js
+++ models/playlist.js
@@ -2,9 +2,22 @@ import mongoose from "mongoose";
 
 
 const playlistSchema = new mongoose.Schema({
-    title: {type: String, required: ['You need to add a playlist title.', true]},
-    director: {type: String, required: ['You need to add a director.', true]},
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: {type: String, required: ['You need to name your playlist.', true]},
+    songs: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Song',
+
+        }],
+    owner: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true },
+    coverArt: {type: String},
+    Bookmarks:{
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+        default: []
+    } 
 }, {
     timestamps: true,
     toJSON: { virtuals: true }
