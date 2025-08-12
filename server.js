9ed6a88c157b22c4bb9414b5caee14d3c2dd444f
Vivien <bihuiy7@gmail.com>
2025-08-12 19:20:42 +1000

    add topSongs into homepage

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ import playlistRouter from "./controllers/playlists.js";
 import songRouter from "./controllers/songs.js";
 import profileRouter from "./controllers/profile.js";
 
-import Playlist from './models/playlist.js';
+import Playlist from "./models/playlist.js";
+import Song from "./models/song.js";
 
 import notFoundHandler from "./middleware/notFoundHandler.js";
 import errorHandler from "./middleware/errorHandler.js";
@@ -23,27 +24,42 @@ app.use(cors({ origin: "http://localhost:5173" }));
 app.use(morgan("dev"));
 
 app.get("/protected-route", verifyToken, (req, res, next) => {
-    return res.json({ message: "HIT PROTECTED ROUTE" });
+  return res.json({ message: "HIT PROTECTED ROUTE" });
 });
 
 app.get("/api", async (req, res, next) => {
-    try {
-        const playlists = await Playlist.find().populate('owner')
-        const popularPlaylists = await Playlist.aggregate([
-        {$addFields: {
-            bookmarkCount: {
-                $size: { $ifNull: [ '$userBookmarks', [] ] }}
-        }
+  try {
+    const popularPlaylists = await Playlist.aggregate([
+      {
+        $addFields: {
+          bookmarkCount: {
+            $size: { $ifNull: ["$userBookmarks", []] },
+          },
         },
-        { $sort: { bookmarkCount: -1 } },
-        { $limit: 4 }
-        ])
-        const topPlaylists = await Playlist.populate(popularPlaylists, 'owner')
-        return res.status(200).json(topPlaylists)
-    } catch (error) {
-        next(error)
-    }
-})
+      },
+      { $sort: { bookmarkCount: -1 } },
+      { $limit: 4 },
+    ]);
+
+    const topSongs = await Song.aggregate([
+      {
+        $addFields: {
+          likeCount: {
+            $size: { $ifNull: ["$userLikes", []] },
+          },
+        },
+      },
+      { $sort: { likeCount: -1 } },
+      { $limit: 4 },
+    ]);
+
+    const topPlaylists = await Playlist.populate(popularPlaylists, "owner");
+
+    return res.status(200).json({ topPlaylists, topSongs });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.use("/api/auth", userRouter);
 app.use("/api/playlists", playlistRouter);
