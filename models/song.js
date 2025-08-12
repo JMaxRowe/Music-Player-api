3c49d5f7464557df6bdcd83325de2b5d49acd281
Vivien <bihuiy7@gmail.com>
2025-08-13 02:00:26 +1000

    add user into profile controller

--- models/song.js
+++ models/song.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const songSchema = new mongoose.Schema({
   title: { type: String, required: [true, `You need to add a song title.`] },
   artist: { type: String, required: [true, `You need to add a artist.`] },
-  duration: { type: Number },
+  duration: { type: String },
   album: { type: String },
   userLikes: {
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
--- models/user.js
+++ models/user.js
@@ -1,44 +1,50 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-    username: { 
-        type: String, 
-        required: ['Please provide a username.', true], 
-        unique: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: ["Please provide a username.", true],
+      unique: true,
     },
-    email: { 
-        type: String, 
-        required: ['Please provide an email.', true], 
-        unique: true 
+    email: {
+      type: String,
+      required: ["Please provide an email.", true],
+      unique: true,
     },
-    password: { 
-        type: String, 
-        required: ['Please provide a password.', true] 
-    }
-},{
-    toJSON: { 
-        virtuals: true, 
-        transform: (doc, objectToBeReturned) => { 
-        delete objectToBeReturned.password
-        delete objectToBeReturned.id
-        }
-    }
-})
+    password: {
+      type: String,
+      required: ["Please provide a password.", true],
+    },
+    profileImage: {
+      type: String,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: (doc, objectToBeReturned) => {
+        delete objectToBeReturned.password;
+        delete objectToBeReturned.id;
+      },
+    },
+  }
+);
 
-userSchema.virtual('createdMovies', {
-    ref: 'Movie',
-    localField: '_id',
-    foreignField: 'author'
-})
+userSchema.virtual("createdMovies", {
+  ref: "Movie",
+  localField: "_id",
+  foreignField: "author",
+});
 
-userSchema.pre('save', function(next){
-    if (this.isModified('password')){
-        this.password = bcrypt.hashSync(this.password, 12)
-    }
-    next()
-})
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, 12);
+  }
+  next();
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
